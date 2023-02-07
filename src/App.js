e0eeb9317465b7d573cf18b7313a35d09ab0fdf9
GuilhermeFujita <fujitaguilherme@gmail.com>
2023-02-06 23:19:32 -0300

    Added Search component and css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { Header } from './components/Header';
+import { Input } from 'antd';
 import './global.css';
 
 import './App.css';
+
+const onSearch = (value) => console.log(value);
+const { Search } = Input;
 
 export function App() {
   return (
     <div className='App'>
       <Header />
       <div className='wrapper'>
-        <h1>Teste</h1>
+        <Search
+          placeholder='input search text'
+          className='searchBox'
+          enterButton='Search'
+          size='large'
+          onSearch={onSearch}
+          style={{
+            width: 783,
+          }}
+        />
       </div>
     </div>
   );
